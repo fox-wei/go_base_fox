bdd2e71dd35a0c8d810a146ce5d5675c633d0198
fox wei <3154527910qq.com>
2021-09-15 10:12:58 +0800

    add Euclids's algorithm at recusrion

--- goalgorithms/src/recursion/dc/dc.go
+++ goalgorithms/src/recursion/dc/dc.go
@@ -7,6 +7,7 @@ import "fmt"
 func main() {
 	total := sum([]int{1, 3, 5, 7, 9})
 	fmt.Println(total)
+	fmt.Println(goc(31415, 14142))
 }
 
 func sum(arr []int) int {
@@ -15,4 +16,14 @@ func sum(arr []int) int {
 	}
 
 	return arr[0] + sum(arr[1:])
+}
+
+//&Euclids's algorithm find the greatest common number
+func goc(m, n int) int {
+	for n != 0 {
+		r := m % n
+		m = n
+		n = r
+	}
+	return m
 }
