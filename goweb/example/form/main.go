df0deafa192ce4e5c97eb300c595eadf603de089
fox wei <3154527910qq.com>
2021-09-26 12:30:52 +0800

    form directory

--- goweb/example/form/main.go
+++ goweb/example/form/main.go
@@ -8,7 +8,7 @@ import (
 )
 
 func sayHello(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()
+	r.ParseForm() //?解析传递参数，如果没有这个无法获取表单数据
 	fmt.Println(r.URL.Path)
 	fmt.Fprintf(w, "Hello ying")
 }
@@ -17,11 +17,18 @@ func login(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Method:", r.Method)
 
 	if r.Method == "GET" {
-		t, _ := template.ParseFiles("login.html")
-		log.Println(t.Execute(w, nil))
+		//&解析html文件
+		t, err := template.ParseFiles("D:/compterstudy/programing_language/go_language/practicalgo/go_base_fox/goweb/example/form/login.html")
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			log.Println(t.Execute(w, nil))
+		}
 	} else {
+		r.ParseForm()
 		fmt.Println("username:", r.Form["username"])
 		fmt.Println("password:", r.Form["password"])
+		fmt.Fprintf(w, "Input, Ok\n")
 	}
 }
 
@@ -29,7 +36,7 @@ func main() {
 	http.HandleFunc("/", sayHello)
 	http.HandleFunc("/login", login)
 
-	err := http.ListenAndServe(":8080", nil)
+	err := http.ListenAndServe(":9090", nil)
 	if err != nil {
 		log.Fatal("ListenAndServer", err)
 	}
