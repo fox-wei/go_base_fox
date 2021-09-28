9b2fd8ae8ccb90433eabdd7a7abaa2a1969a33ec
fox wei <3154527910qq.com>
2021-09-28 14:40:56 +0800

    add the verify for form

--- goweb/example/form/formmain/main.go
+++ goweb/example/form/formmain/main.go
@@ -6,6 +6,8 @@ import (
 	"net/http"
 	"strconv"
 	"text/template"
+
+	"github.com/fox-wei/go_base_fox/goweb/example/form/verify"
 )
 
 func sayHello(w http.ResponseWriter, r *http.Request) {
@@ -28,14 +30,11 @@ func login(w http.ResponseWriter, r *http.Request) {
 	} else {
 		r.ParseForm() //^解析参数，否则无法获取请求信息
 		// fmt.Println("username:", r.Form.Get("username")) //*如果是map r.Form["name"]
-		getInt, err := strconv.Atoi(r.Form.Get("username"))
-		if err != nil {
-			fmt.Fprintln(w, err.Error())
-		} else {
-			fmt.Println("username:", getInt)
-			fmt.Println("password:", r.Form.Get("password"))
-			fmt.Fprintf(w, "Input, Ok\n")
-		}
+		getInt, _ := strconv.Atoi(r.Form.Get("username"))
+		fmt.Println("username:", getInt)
+		fmt.Println("password:", r.Form.Get("password"))
+		fmt.Fprintf(w, "Input, Ok\n")
+
 	}
 }
 
@@ -50,10 +49,47 @@ func logon(w http.ResponseWriter, r *http.Request) {
 			log.Println(t.Execute(w, nil))
 		}
 	} else {
-		r.ParseForm() //*解析参数获取请求信息
+		r.ParseForm()               //*解析参数获取请求信息
+		errs := map[string]string{} //?保存错误信息
+
+		//!验证输入信息
 		name := r.Form.Get("username")
+		if !verify.IsChinese(name) {
+			errs["name"] = name + " is must full Chinese"
+		}
+
 		english := r.Form.Get("englishname")
-		fmt.Println(name, english)
+		if !verify.IsEnglish(english) {
+			errs["english"] = english + " is must full English"
+		}
+
+		id := r.Form.Get("id")
+		if !verify.IsID(id) {
+			errs["id"] = id + " is not 15 or 18 numbers"
+		}
+
+		phone := r.Form.Get("telephone")
+		if !verify.IsMobile(phone) {
+			errs["phone"] = phone + " is wrong"
+		}
+
+		email := r.Form.Get("email")
+		if !verify.IsEmail(email) {
+			errs["email"] = email + " is wrong"
+		}
+
+		sex := r.Form.Get("gender")
+		ch := r.Form.Get("check")
+		if len(errs) != 0 {
+			var info string = ""
+			for _, v := range errs {
+				info = info + v + "\n"
+			}
+			fmt.Fprintln(w, info)
+		} else {
+			fmt.Fprintln(w, "Input OK")
+			fmt.Println(name, english, id, phone, email, sex, ch)
+		}
 
 	}
 }
