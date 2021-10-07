b8d6f6e328b0dd632784e5b09a6e2a17f3497277
fox wei <3154527910qq.com>
2021-10-07 10:27:39 +0800

    add new comment

--- goweb/example/form/loadfile/upload.go
+++ goweb/example/form/loadfile/upload.go
@@ -16,11 +16,13 @@ func UploadFile(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Method:", r.Method)
 
 	if r.Method == "GET" {
+		//*Set the type of hidden in form in order to make srue submit one time
 		crutime := time.Now().Unix()
 		h := md5.New()
 		io.WriteString(h, strconv.FormatInt(crutime, 10))
 		token := fmt.Sprintf("%x", h.Sum(nil))
 
+		//*Show the html in broswer
 		t, err := template.ParseFiles("D:\\compterstudy\\programing_language\\go_language\\practicalgo\\go_base_fox\\goweb\\example\\form\\loadfile\\upload.html")
 		if err != nil {
 			fmt.Fprintln(w, err)
@@ -37,13 +39,14 @@ func UploadFile(w http.ResponseWriter, r *http.Request) {
 			defer file.Close()
 			fmt.Fprintf(w, "%v", handler.Header)
 
+			//^Copy the file from broser
 			f, err := os.OpenFile("D:\\compterstudy\\programing_language\\go_language\\practicalgo\\go_base_fox\\goweb\\example\\form\\loadfile\\test\\"+handler.Filename, os.O_WRONLY|os.O_CREATE, 0666)
-
 			if err != nil {
 				fmt.Println(err)
 			} else {
 				defer f.Close()
 				io.Copy(f, file) //*文件复制到服务端
+				fmt.Printf("The file %s have save\n", handler.Filename)
 			}
 		}
 	}
