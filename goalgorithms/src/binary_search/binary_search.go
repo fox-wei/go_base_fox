8b34447b268a9d3d5e46cc58c435ad118113016c
fox wei <3154527910qq.com>
2021-10-29 20:49:14 +0800

    add sort by three

--- goalgorithms/src/binary_search/binary_search.go
+++ goalgorithms/src/binary_search/binary_search.go
@@ -11,24 +11,98 @@ func BinarySearch(list []int, target int) int {
 
 	step := 0 //^记录查找次数
 
-	for {
+	for low <= high {
 		step = step + 1
-		if low <= high { //!容易出错
-			mid := (low + high) / 2 //*从中间开始
-			guess := list[mid]
 
-			//*找到目标值
-			if guess == target {
-				fmt.Printf("共查找了%d次\n", step)
-				return mid
-			}
+		mid := (low + high) / 2 //*从中间开始
+		guess := list[mid]
 
-			//*目标值小于中间值
-			if guess > target {
-				high = mid - 1
-			} else {
-				low = mid + 1
-			}
+		//*找到目标值
+		if guess == target {
+			fmt.Printf("在%d找到,共查找了%d次\n", mid, step)
+			return mid
+		}
+
+		//*目标值小于中间值
+		if guess > target {
+			high = mid - 1
+		} else {
+			low = mid + 1
 		}
 	}
+	fmt.Printf("%d找不到，共查找了%d次\n", target, step)
+	return -1
+}
+
+//^三分查找
+func TrisectionSearch(arr []int, val int) (int, int) {
+	low := 0
+	high := len(arr) - 1
+
+	step := 0 //^记录查找次数
+
+	for low <= high {
+		step += 1
+		mid1 := low + int((high-low)/3)
+		mid2 := high - int((high-low)/3)
+
+		// 枢轴
+		midData1 := arr[mid1]
+		midData2 := arr[mid2]
+
+		if midData1 == val {
+			return mid1, step
+		} else if midData2 == val {
+			return mid2, step
+		}
+
+		if midData1 < val {
+			low = mid1 + 1
+		} else if midData2 > val {
+			high = mid2 - 1
+		} else {
+			low = low + 1
+			high = high - 1
+		}
+	}
+	return -1, step
+}
+
+//^插值插入
+func MidSearch(arr []int, val int) (int, int) {
+
+	low := 0
+	high := len(arr) - 1
+
+	step := 0
+	//循环的终止条件
+	for low <= high {
+		step += 1
+
+		// 大段
+		leftv := float64(val - arr[low])
+
+		// 整段
+		allv := float64(arr[high] - arr[low])
+
+		// 整段差
+		diff := float64(high - low)
+
+		// 计算中间值
+		mid := int(float64(low) + diff*leftv/allv)
+
+		if mid < 0 || mid >= len(arr) {
+			return -1, step
+		}
+
+		if arr[mid] > val {
+			high = mid - 1
+		} else if arr[mid] < val {
+			low = mid + 1
+		} else {
+			return mid, step
+		}
+	}
+	return -1, step
+
 }
--- goalgorithms/src/binary_search/binary_search_test.go
+++ goalgorithms/src/binary_search/binary_search_test.go
@@ -7,7 +7,13 @@ import (
 
 func TestBinarySearch(t *testing.T) {
 	s := []int{22, 23, 24, 26, 1301, 1602, 1901, 1902} //?有序
-	fmt.Println("寻找值22的位置:")
-	res := BinarySearch(s, 22)
-	fmt.Printf("所在位置为%d\n", res)
+	fmt.Println("寻找值26的位置:")
+	res := BinarySearch(s, 26)
+	fmt.Printf("二分查找所在位置为%d\n", res)
+
+	s1, s2 := TrisectionSearch(s, 26)
+	fmt.Printf("三分查找，所在位置为%d，比较次数:%d\n", s1, s2)
+
+	s3, s4 := MidSearch(s, 126)
+	fmt.Printf("插值查找，所在位置为%d，比较次数:%d\n", s3, s4)
 }
