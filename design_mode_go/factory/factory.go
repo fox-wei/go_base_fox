7a80d2e3105a666219df4c385a79965091ab1166
fox wei <3154527910qq.com>
2022-05-08 15:21:42 +0800

    add notes

--- design_mode_go/factory/factory.go
+++ design_mode_go/factory/factory.go
@@ -13,12 +13,14 @@ func (c Chinese) MyFavoriteFood() {
 	fmt.Println("Chinese like eating the rice and noodles")
 }
 
+//*产品
 type American struct{}
 
 func (a American) MyFavoriteFood() {
 	fmt.Println("American like eating hamburger")
 }
 
+//^工厂
 func NewEatFool(name string) EatFool {
 	switch name {
 	case "c":
