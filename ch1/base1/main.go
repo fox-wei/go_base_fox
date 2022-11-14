bb03c55e313622de8d7a5aa3587d00573e22f613
fox wei <3154527910qq.com>
2022-11-14 16:03:31 +0800

    add new change

--- ch1/base1/main.go
+++ ch1/base1/main.go
@@ -1,27 +1,38 @@
 package main
 
-import (
-	"fmt"
-	"unicode/utf8"
-)
+import "fmt"
 
 func main() {
-	fmt.Println("Hello Go!")
-	s := "我的专业是软件工程"
-	fmt.Printf("len of the bytes:%d\n", len(s))
-	fmt.Printf("len of the char:%d\n", utf8.RuneCountInString(s))
+	// fmt.Println("Hello Go!")
+	// s := "我的专业是软件工程"
+	// fmt.Printf("len of the bytes:%d\n", len(s))
+	// fmt.Printf("len of the char:%d\n", utf8.RuneCountInString(s))
 
-	r := &replaceHolder{"fox", 21}
-	fmt.Printf("v->%v\n", r)
-	fmt.Printf("+v->%+v\n", r)
-	fmt.Printf("#v->%#v\n", r)
+	// r := &replaceHolder{"fox", 21}
+	// fmt.Printf("v->%v\n", r)
+	// fmt.Printf("+v->%+v\n", r)
+	// fmt.Printf("#v->%#v\n", r)
 
-	m := []int{0, 1, 2, 3, 4, 5}
-	m1 := m[1:4]
-	fmt.Printf("m:%v\tm1:%v\n", m, m1)
+	// m := []int{0, 1, 2, 3, 4, 5}
+	// m1 := m[1:4]
+	// fmt.Printf("m:%v\tm1:%v\n", m, m1)
+
+	twoTimes := partitalTimes(2)
+	fmt.Println(twoTimes(5))
+	fmt.Println(twoTimes(6))
 }
 
 type replaceHolder struct {
 	Name string
 	Age  int
+}
+
+func times(x, y int) int {
+	return x * y
+}
+
+func partitalTimes(x int) func(int) int {
+	return func(i int) int {
+		return times(x, i)
+	}
 }
--- ch1/try/tru.go
+++ ch1/try/tru.go
@@ -34,6 +34,27 @@ func showRectangle(width, weigth int) {
 	}
 }
 
+func compareRange() {
+	var a = [5]int{1, 2, 3, 4, 5}
+	var r [5]int
+
+	fmt.Println("original a=", a)
+
+	for i, v := range a {
+		if i == 0 {
+			a[1] = 12
+			a[2] = 13
+		}
+
+		r[i] = v
+	}
+
+	fmt.Println("after the change a=", a)
+	fmt.Println("after the change r=", r)
+}
+
 func main() {
 	showRectangle(20, 10)
+
+	compareRange()
 }
